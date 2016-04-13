ac3336069ffc2bb597c56f6b515258e876e86386
JuanAlejandro <jalejandro.hperez@gmail.com>
2016-04-13 12:45:37 -0400

    starting to implement list of hotels

---
+++ Hoteles Cubanacan/src/com/resline/cubanacan/ui/activities/HotelesListActivity.java
@@ -0,0 +1,9 @@
+package com.resline.cubanacan.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Juan Alejandro on 13/04/2016.
+ */
+public class HotelesListActivity extends AppCompatActivity{
+}
