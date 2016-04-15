5ae7f67cf6bd8e92b3e10255be5f4f05a1567aa3
jalejandro.hperez@gmail.com <JuanAlejandro>
2016-04-14 22:35:48 -0400

    Android-ObservableScrollView module added and showing hotel details (only activity and base layout)

--- Hoteles Cubanacan/src/com/resline/cubanacan/ui/activities/HotelDetailsActivity.java
+++ Hoteles Cubanacan/src/com/resline/cubanacan/ui/activities/HotelDetailsActivity.java
@@ -1,24 +1,46 @@
 package com.resline.cubanacan.ui.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import com.resline.cubanacan.R;
 import com.resline.cubanacan.ui.activities.api.BaseActivity;
 
 /**
  * Created by Juan Alejandro on 13/04/2016.
  */
-public class HotelDetailsActivity extends BaseActivity{
+public class HotelDetailsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // to set back arrow in toolbar
+        setToolbar();
+    }
+
+    private void setToolbar() {
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
     @Override
     protected int getLayoutResourceIdentifier() {
-        return 0;
+        return R.layout.activity_details;
     }
 
     @Override
     protected String getTitleToolBar() {
-        return null;
+        return "";
     }
 
     @Override
     protected Toolbar getLayoutResourceToolBar() {
-        return null;
+        return (Toolbar) findViewById(R.id.toolbar);
     }
 }
