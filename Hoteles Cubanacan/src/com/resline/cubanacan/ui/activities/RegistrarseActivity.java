816250182949cf62798037c7a9adffa320b77566
JuanAlejandro <jalejandro.hperez@gmail.com>
2016-04-25 00:24:18 -0400

    sing up flow almost finished

--- Hoteles Cubanacan/src/com/resline/cubanacan/ui/activities/RegistrarseActivity.java
+++ Hoteles Cubanacan/src/com/resline/cubanacan/ui/activities/RegistrarseActivity.java
@@ -1,20 +1,32 @@
 package com.resline.cubanacan.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import com.resline.cubanacan.R;
 import com.resline.cubanacan.ui.activities.api.BaseActivity;
 
 /**
  * Created by Juan Alejandro on 24/04/2016.
  */
-public class RegistrarseActivity extends BaseActivity {
+public class RegistrarseActivity extends BaseActivity implements View.OnClickListener {
+    private Button btnAceptar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setToolBar();
+
+        loadViewComponents();
+    }
+
+    private void loadViewComponents() {
+        btnAceptar = (Button) findViewById(R.id.accept);
+        btnAceptar.setOnClickListener(this);
     }
 
     @Override
@@ -41,5 +53,14 @@ public class RegistrarseActivity extends BaseActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.accept:
+                startActivity(new Intent(RegistrarseActivity.this, UserProfileActivity.class));
+                break;
+        }
     }
 }
